7df169e8ef01da5ac6b58ea8cecbe24b3df86dd6
dengzi <dengzii@foxmail.com>
2019-12-26 23:40:29 +0800

    user router

--- routers/routers.go
+++ routers/routers.go
@@ -3,15 +3,12 @@ package routers
 import (
 	"github.com/kataras/iris"
 	"github.com/kataras/iris/context"
-	"github.com/kataras/iris/hero"
 	"log"
-	"reflect"
 	"server/bootstrap"
 	"server/controllers"
 	"server/controllers/article"
 	"server/controllers/common"
 	"server/controllers/user"
-	"strings"
 )
 
 func Setup(app *bootstrap.Bootstrapper) {
@@ -20,38 +17,21 @@ func Setup(app *bootstrap.Bootstrapper) {
 
 	mainRouter := app.Party("/")
 
-	macro := func(paramValue string) (i interface{}, b bool) {
-		return strings.Split(paramValue, "/"), true
-	}
-
-	app.Macros().Register("username", "",
-		false, true, macro).
-		RegisterFunc("contains", func(expectedItem []string) func(paramValue []string) bool {
-			return func(paramValue []string) bool {
-				return true
-			}
-		})
-
-	context.ParamResolvers[reflect.TypeOf([]string{})] = func(paramIndex int) interface{} {
-		return func(ctx iris.Context) []string {
-			return ctx.Params().GetEntryAt(paramIndex).ValueRaw.([]string)
-		}
-	}
-
-	app.Handle("GET", "/test/{a:username}", hero.Handler(func(param []string) string {
-		return "params, " + param[0] + param[1]
-	}))
-
-	app.Get("/test/{myparam:username contains([value1,value2])}", func(ctx iris.Context) {
-		// When it is not a builtin function available to retrieve your value with the type you want, such as ctx.Params().GetInt
-		// then you can use the `GetEntry.ValueRaw` to get the real value, which is set-ed by your macro above.
-		myparam := ctx.Params().GetEntry("myparam").ValueRaw.([]string)
-		_, _ = ctx.Writef("myparam's value (a trailing path parameter type) is: %#v\n", myparam)
-	})
-
 	userRouter := mainRouter.Party("/{username:alphabetical}")
 
 	userRouter.Handle("GET", "/", catchErrorRouter(controllers.HomeController))
+
+	userArticle := userRouter.Party("/articles")
+	userArticle.Handle("GET", "/", func(i context.Context) {
+		_, _ = i.WriteString("get article from user: " + i.Params().GetString("username"))
+	})
+
+	userArticle.Handle("GET", "/", func(i context.Context) {
+		_, _ = i.WriteString("get article from user: " + i.Params().GetString("username"))
+	})
+	userArticle.Handle("GET", "/{id:uint}", func(i context.Context) {
+		_, _ = i.WriteString("get article id " + i.Params().GetString("id") + ", from user " + i.Params().GetString("username"))
+	})
 
 	app.Handle("GET", "/home", catchErrorRouter(controllers.HomeController))
 
