2fd20454c824fde1a65d55b6f7e0ac19af02dde2
dengzi <dengzii@foxmail.com>
2020-04-12 21:36:25 +0800

    allow cros

--- controllers/common/authority.go
+++ controllers/common/authority.go
@@ -14,6 +14,11 @@ func AuthorityController(ctx context2.Context) {
 
 	//path := ctx.Path()
 	//authorityUrl[path]
-
+	ctx.Header("Vary", "Access-Control-Request-Method")
+	ctx.Header("Access-Control-Allow-Origin", "*")
+	ctx.Header("Access-Control-Allow-Credentials", "true")
+	ctx.Header("Access-Control-Allow-Methods", "*")
+	ctx.Header("Access-Control-Request-Headers", "Accept,content-type,X-Requested-With,Content-Length,Accept-Encoding,X-CSRF-Token,Authorization,token")
+	ctx.Header("Access-Control-Request-Method", "*")
 	ctx.Next()
 }
